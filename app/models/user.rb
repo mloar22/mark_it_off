d842184bf8de6412a7899beb5e36ce5ca75b75c4
Mitch Loar <mitchloarweb@gmail.com>
2016-04-01 08:13:11 -0700

    checkpoints 4-8 complete, avatar removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_many :items
-  mount_uploader :avatar, AvatarUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
